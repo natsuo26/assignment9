3e493bfee032bf45afc76b7057c5b978c07c8e7b
u31d81 <u31d81@INGURWN130183>
2023-11-01 15:14:22 +0530

    Add project files.

---
+++ assignment9/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Data.SqlTypes;
+
+namespace assignment9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Shape shape=null;
+            Console.WriteLine("Enter shape type");
+            int c = Byte.Parse(Console.ReadLine());
+            if (c == 1)
+            {
+                shape = new Square();
+            }
+            else if(c==2)
+            {
+                shape = new Rectangle();
+            }
+            
+            Check ch = new Check();
+            ch.CheckShape(shape);
+        }
+    }
+    class Check
+    {
+        public void CheckShape(Shape shape)
+        {
+            if(shape is Square)
+            {
+                foo((Square)shape);     
+            }
+            else if(shape is Rectangle)
+            {
+                foo((Rectangle)shape);
+            }
+            
+        }
+        public void foo(Square square)
+        {
+            square.Sq();
+            square.GetDimensions();
+            square.CalculateArea();
+            square.DisplayArea();
+        }
+        public void foo(Rectangle rectangle)
+        {
+            rectangle.Re();
+            rectangle.GetDimensions();
+            rectangle.CalculateArea();
+            rectangle.DisplayArea();
+        }
+    }
+}
---
+++ assignment9/Shape.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment9
+{
+    abstract class Shape
+    {
+        protected int area;
+        public abstract void GetDimensions();
+        public abstract void CalculateArea();
+        public void DisplayArea()
+        {
+            Console.WriteLine("Area is " + area);
+        }
+    }
+    class Square : Shape
+    {
+        int side;
+
+        public override void CalculateArea()
+        {
+            area = side * side;
+        }
+
+        public override void GetDimensions()
+        {
+            Console.WriteLine("Enter side");
+            side = Byte.Parse(Console.ReadLine());
+        }
+        public void Sq()
+        {
+            Console.WriteLine("this is a sqaure");
+        }
+    }
+    class Rectangle : Shape
+    {
+        int length, breadth;
+
+        public override void GetDimensions()
+        {
+            Console.WriteLine("Enter length");
+            length = Byte.Parse(Console.ReadLine());
+            Console.WriteLine("Enter breadth");
+            breadth = Byte.Parse(Console.ReadLine());
+        }
+        public override void CalculateArea()
+        {
+            area = length * breadth;
+        }
+        public void Re()
+        {
+            Console.WriteLine("this is a Rectangle");
+        }
+
+    }
+
+}
